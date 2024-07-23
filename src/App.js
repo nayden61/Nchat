6e33823323ad40fb47bd5bae2a3c7cf29de89379
Nayden <nayden61@gmail.com>
2024-07-23 03:26:17 +0100

    Starter code including ChatEngine

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {ChatEngine} from 'react-chat-engine';
+
+import './App.css';
+
+export default function App() {
+    return (
+        <ChatEngine 
+            height= "100vh"
+            projectID = "eaabba16-9c43-4c40-ab75-386110970c94"
+            userName = "John"
+            userSecret = "" />
+    )
+}
